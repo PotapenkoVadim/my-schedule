d884e0f016f41f47e6b380c6569caae3f0882c49
Potapenko Vadim <vpotapenko@studio-tg.ru>
2023-11-20 15:13:08 +0300

    feat: implement error page

--- src/views/Error/Error.tsx
+++ src/views/Error/Error.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import cn from "classnames";
+import { RouterMap } from "@/constants";
+import {Button} from "@/components";
+import styles from "./Error.module.scss";
+
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  const iconClasses = cn("pi pi-info-circle", styles.error__icon);
+  const goBack = () => navigate(`/${RouterMap.Calendar}`);
+
   return (
-    <div>Error page</div>
+    <div className={styles.error}>
+      <div className={styles.error__content}>
+        <span className={iconClasses}></span>
+        <h1>Something went wrong!</h1>
+      </div>
+
+      <Button onClick={goBack}>Go back</Button>
+    </div>
   );
 }
