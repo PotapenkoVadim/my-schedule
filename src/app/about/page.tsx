854d1a50cd14ec4390a4b32630cd2d68cad7ea01
Potapenko Vadim <vpotapenko@studio-tg.ru>
2024-09-15 16:02:59 +0300

    feat: update about page display

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,54 +1,71 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { ShapesBackground, Spinner } from "@/components";
+import { ABOUT_PAGE_IMAGES, APP_TITLE, CURRENT_YEAR } from "@/constants";
 import { useAppContext } from "@/context";
-import { Gallery, UserGenerator } from "@/libs";
-import { PageContent, Spinner } from "@/components";
-import { ABOUT_PAGE_IMAGES, CURRENT_YEAR } from "@/constants";
 import { useSession } from "@/hooks";
-import { useEffect } from "react";
 import { getToken } from "@/utils";
+import { Gallery, UserGenerator } from "@/libs";
 import styles from "./page.module.scss";
 
 export default function AboutPage() {
   const { theme } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
   const { currentUser, isSessionLoading, getSession } = useSession();
 
   useEffect(() => {
     const token = getToken();
-
     if (!currentUser && token) {
       getSession(CURRENT_YEAR);
     }
+
+    setIsLoading(false);
   }, [currentUser]);
 
   let content;
-  if (isSessionLoading) {
+  if (isSessionLoading || isLoading) {
     content = <Spinner />;
   } else {
     content = (
-      <PageContent>
-        <h1 className={styles.page__title}>My Schedule</h1>
+      <>
+        <ShapesBackground />
 
-        <div className={styles.page__text}>
-          Приложение предназначено для учета и визуализации заказов. Оно
-          позволяет пользователям просматривать информацию о заказах в двух
-          форматах: в виде цветовых схем на календаре и в виде таблицы. Каждый
-          заказ имеет свой цвет, который выбирает пользователь, и статус,
-          который может меняться со временем. Статус заказа отображается на
-          календаре различными способами в зависимости от его текущего
-          состояния.
-        </div>
-        <div className={styles.page__text}>
-          Приложение также поддерживает ролевую модель с тремя уровнями доступа:
-          администратор, пользователь и временный пользователь. Администратор
-          имеет возможность добавлять новых пользователей. Пользователи и
-          временные пользователи имеют доступ к основным функциям приложения, но
-          доступ временных пользователей к приложению ограничен сутками.
-        </div>
+        <div className={styles.page__content}>
+          <div>
+            <h1 className={styles.page__title}>{APP_TITLE} App.</h1>
 
-        <UserGenerator className={styles.page__generator} />
-        <Gallery className={styles.page__gallery} images={ABOUT_PAGE_IMAGES} />
-      </PageContent>
+            <div className={styles.page__description}>
+              Приложение предназначено для учета и визуализации заказов. Оно
+              позволяет пользователям просматривать информацию о заказах в двух
+              форматах: в виде цветовых схем на календаре и в виде таблицы.
+              Каждый заказ имеет свой цвет, который выбирает пользователь, и
+              статус, который может меняться со временем. Статус заказа
+              отображается на календаре различными способами в зависимости от
+              его текущего состояния.
+            </div>
+
+            <div className={styles.page__subdescription}>
+              Приложение также поддерживает ролевую модель с тремя уровнями
+              доступа: администратор, пользователь и временный пользователь.
+              Администратор имеет возможность добавлять новых пользователей.
+              Пользователи и временные пользователи имеют доступ к основным
+              функциям приложения, но доступ временных пользователей к
+              приложению ограничен сутками.
+            </div>
+
+            <UserGenerator className={styles.page__generator} />
+          </div>
+
+          <div className={styles.page__wrapper}>
+            <Gallery
+              theme={theme}
+              className={styles.page__gallery}
+              images={ABOUT_PAGE_IMAGES}
+            />
+          </div>
+        </div>
+      </>
     );
   }
 
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,22 +1,46 @@
 "use client";
 
 import { useAppContext } from "@/context";
-import { ShapesBackground, Button } from "@/components";
+import { ShapesBackground, Button, Spinner } from "@/components";
+import { useSession } from "@/hooks";
+import { getToken } from "@/utils";
+import { useEffect } from "react";
+import { CURRENT_YEAR } from "@/constants";
 import styles from "./page.module.scss";
 
 export default function Error({ reset }: { reset: () => void }) {
   const { theme } = useAppContext();
+  const { currentUser, isSessionLoading, getSession } = useSession();
+
+  useEffect(() => {
+    const token = getToken();
+
+    if (!currentUser && token) {
+      getSession(CURRENT_YEAR);
+    }
+  }, [currentUser]);
+
+  let content;
+  if (isSessionLoading) {
+    content = <Spinner />;
+  } else {
+    content = (
+      <>
+        <ShapesBackground />
+
+        <div className={styles.page__content}>
+          <h1>Ой, что-то пошло не так!</h1>
+          <Button size="large" onClick={reset}>
+            Повторить запрос
+          </Button>
+        </div>
+      </>
+    );
+  }
 
   return (
     <main data-theme={theme} className={styles.page}>
-      <ShapesBackground />
-
-      <div className={styles.page__content}>
-        <h1>Ой, что-то пошло не так!</h1>
-        <Button size="large" onClick={reset}>
-          Повторить запрос
-        </Button>
-      </div>
+      {content}
     </main>
   );
 }
