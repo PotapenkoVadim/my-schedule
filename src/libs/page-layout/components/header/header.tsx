9b25d9be666e2dce30b0ed55d3367d802a2c0da3
Potapenko Vadim <vpotapenko@studio-tg.ru>
2024-10-02 20:58:53 +0300

    fix: semantic & styles in layout

--- src/libs/page-layout/components/header/header.tsx
+++ src/libs/page-layout/components/header/header.tsx
@@ -2,5 +2,5 @@ import { ThemeVariant } from "@/types";
 import styles from "./header.module.scss";
 
 export function Header({ theme }: { theme: ThemeVariant }) {
-  return <div data-theme={theme} className={styles.header} />;
+  return <header data-theme={theme} className={styles.header} />;
 }
